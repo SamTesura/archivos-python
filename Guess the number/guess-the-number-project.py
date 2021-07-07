296db34c20b66543e31d7cacf55a8aa55a327f2d
Ricardo Ovalle <ricardoovallesantos@gmail.com>
2021-07-07 13:11:08 -0400

    Guess the game - project
    Finished
    Commented
    Optimized

---
+++ Guess the number/guess-the-number-project.py
@@ -0,0 +1,76 @@
+import random
+import os
+from assets import logo
+
+def error_int():
+    print("ERROR! Intentelo de nuevo.")
+
+def win(the_correct_number):
+    print(f"You got it! The answer was {the_correct_number}.")
+
+def lose():
+    print("You've run out of guesses, you lose.")
+
+
+#-----------------------------------------------------------------
+
+#Main program
+def guess_the_number():
+    print("hi")
+    os.system('cls')
+    print(logo)
+    print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+
+    #Randomly chose a number between 1 and 100. The correct answer
+    the_number = random.randint(1, 100)
+
+    while True:     #Loop to only accpet 'y' o 'n'
+        dif_question = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+        if dif_question == "easy" or dif_question == "hard":
+            break
+        else:
+            error_int()
+
+    #Deifficulty. Hard: 5 attempts, Easy: 10 attempts
+    if dif_question == 'easy':
+        attempts = 10
+    elif dif_question == 'hard':
+        attempts = 5
+
+    while True:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        guessed = int(input("Make a guess: "))
+        
+        #comparison to see if it's equal to the correct answer or not
+        #if guessed the answer wins
+        if guessed == the_number:
+            win(the_number)
+            break
+        #Lose a life if incorrect
+        elif guessed != the_number:
+            attempts -= 1  
+            if guessed > the_number:
+                print("Too high.")
+            elif guessed < the_number:
+                print("Too low.")
+        #If has no more attempts, lose
+        if attempts != 0:
+            print("Guess again")
+        else:
+            lose()
+            break
+
+    #THE FINAL QUESTION
+    while True:     #Loop to only accpet 'y' o 'n'
+        question = input("Would you like to play again? Type 'y' for yes, type 'n' to pass: ").lower()
+        if question == "y" or question == "n":
+            break
+        else:
+            error_int()
+
+    if question == 'y':
+        guess_the_number()
+        #Recursion. Call my function inside of itseld to start again from scratch
+            
+#Call my function. Main program            
+guess_the_number()
