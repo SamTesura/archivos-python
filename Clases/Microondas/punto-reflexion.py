19a0d7e678c7798b0487b881f629d3c9d25279bf
Ricardo Ovalle <ricardoovallesantos@gmail.com>
2021-07-08 08:55:35 -0400

    Update punto-reflexion.py

--- Clases/Microondas/punto-reflexion.py
+++ Clases/Microondas/punto-reflexion.py
@@ -3,10 +3,10 @@ import os
 def error_int():
     print("ERROR! Intentelo de nuevo.")
 
-while True:
+def punto_reflexion():
+# while True:
     os.system('cls')
     
-
     while True:     #Loop to only accept floating
         h1 = input("h1: ")
         try: 
@@ -32,29 +32,33 @@ while True:
     a = 6370
 
     c = (h1-h2)/(h1+h2)
-    print(f"c = {c}")
+    print(f"\nc = {c}")
     m = (((d)**2)/(4*k*a*((0.001*h1)+(0.001*h2))))
     print(f"m = {m}")
 
     while True:     #Loop to only accept floating
-        b = input("Buscar el parámetro b en la tabla. b : ")
+        b = input("\nBuscar el parámetro b en la tabla. b : ")
         try: 
             b = float(b)
             break
         except: error_int()
 
     d1 = (d/2)*(1+b)
-    print(f"d1 = {d1}")
+    print(f"\nd1 = {d1}")
     d2 = (d/2)*(1-b)
     print(f"d2 = {d2}")
 
     #THE FINAL QUESTION
-    while True:     #Loop to only accpet 'y' o 'n'
-        question = input("Would you like to play again? Type 'y' for yes, type 'n' to pass: ").lower()
-        if question == "y" or question == "n":
+    while True:     #Loop to only accpet 'si' o 'no'
+        question = input("\nQuieres buscar el punto de reflexión nuevamente?\nEscribe 'si' o 'no': ").lower()
+        if question == "si" or question == "no":
             break
         else:
             error_int()
 
-    if question == 'n':
-        break
+    if question == 'si':
+        # break
+        punto_reflexion()
+        #Recursion. Call my function inside of itseld to start again from scratch
+
+punto_reflexion()
